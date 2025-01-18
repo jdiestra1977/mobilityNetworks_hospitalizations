75972f393f1585a782941e93a52abe3c83f0cf10
Jose Herrera <diestra@gmail.com>
2025-01-17 23:21:32 -0600

    Initial code and sample data

---
+++ mobilityNetworks.R
@@ -0,0 +1,81 @@
+library(tidyverse)
+library(sf)
+library(tidycensus)
+library(dplyr)
+library(igraph)
+source("networkFunctions.R")
+
+setwd("~/Documents/GitHub/mobilityNetworks_hospitalizations/")
+#
+#Getting mapping cbgs - zip code -------
+# Load data for population by ZCTA in Harris County
+zip_codes_TX_2019 <- get_acs(
+  geography = "zcta",
+  variables = "B01003_001", # Total population
+  state = "TX",
+  year = 2019, # Change to the desired year
+  survey = "acs5"
+)
+
+US_population_2022 <- get_acs(
+  geography = "zcta",
+  variables = "B01003_001", # Total population
+  year = 2022, # Change to the desired year
+  survey = "acs5",
+  geometry=T
+)
+
+US_population_2022 %>% filter(GEOID %in% zip_codes_TX_2019$GEOID)
+
+cbg_shapes <- tigris::block_groups(state = "TX", year = 2021, cb = TRUE) # Replace "TX" with your state
+zcta_shapes <- tigris::zctas(state="TX",year = 2021)
+
+zip_codes <- c("77002", "77003", "77004") # Replace with your list of ZIP codes
+zcta_filtered <- zcta_shapes %>%
+  filter(ZCTA5CE20 %in% zip_codes_TX_2019$GEOID)
+
+cbg_in_zips <- st_intersection(cbg_shapes, zcta_filtered)
+
+cbg_zip_mapping_TX<-cbg_in_zips %>% as_tibble() %>%
+  select(cbgs=GEOID,ZCTA=ZCTA5CE20) %>%
+  st_drop_geometry() %>% unique()
+
+save(cbg_zip_mapping_TX,file="cbg_zip_mapping_TX.RData")
+###
+
+## Processing visits - from cbgs to zip codes ------
+load("cbg_zip_mapping_TX.RData")
+
+cbg_zip_mapping_TX
+
+#This file contains visits between cbgs and labels for NAICS and place
+visits_sample<-read_csv("visits_cbg_to_cbg_TX_2018-08-06.csv")
+fecha_week<-"visits_cbg_to_cbg_TX_2018-08-06.csv" %>% str_split(.,"TX_") %>% unlist(.) %>% 
+  str_subset(.,"csv") %>% str_remove(.,".csv")
+#Making the netowrk
+graph_zip<-make_net_zipcode(visits_sample)
+#Adding different normalizations
+graph_zip<-diff_normalized_weights(graph_zip)
+#I need to save each graph in a list, with the week as name
+
+#These are for each normalization for the weights of the network
+
+norms<-c("totalVisits","weight_inv","weight_range","weight_sum","weight_max")
+
+#I uploaded and fix this file in TACC to create networks and get statistics
+
+#Metric for each node and normalization
+bind_rows(node_metrics(graph_zip,norms[1]) %>% mutate(Nomal=norms[1],Week=fecha_week),
+          node_metrics(graph_zip,norms[2]) %>% mutate(Nomal=norms[2],Week=fecha_week),
+          node_metrics(graph_zip,norms[3]) %>% mutate(Nomal=norms[3],Week=fecha_week),
+          node_metrics(graph_zip,norms[4]) %>% mutate(Nomal=norms[4],Week=fecha_week),
+          node_metrics(graph_zip,norms[5]) %>% mutate(Nomal=norms[5],Week=fecha_week)) %>%
+  as.data.frame(row.names = 1:nrow(.))
+
+#Metric for ach network and normalization
+bind_rows(network_metrics(graph_zip,norms[1]) %>% mutate(Normal=norms[1],Week=fecha_week),
+         network_metrics(graph_zip,norms[2]) %>% mutate(Normal=norms[2],Week=fecha_week),
+         network_metrics(graph_zip,norms[3]) %>% mutate(Normal=norms[3],Week=fecha_week),
+         network_metrics(graph_zip,norms[4]) %>% mutate(Normal=norms[4],Week=fecha_week),
+         network_metrics(graph_zip,norms[5]) %>% mutate(Normal=norms[5],Week=fecha_week))
+
---
+++ networkFunctions.R
@@ -0,0 +1,84 @@
+
+#I am commenting this function, since our most granular resolution are Zip codes
+#Function to build a network for cbgs. Needs x="file with visits".
+#This file must contain, at least, Week, home_cbg, dest_cbg, and totalVisits
+# make_net_cbgs<-function(x){
+#   edgelist_cbgs <- x %>%
+#     select(Week, contains("cbg"), totalVisits) %>%
+#     group_by(Week, home_cbg, dest_cbg) %>%
+#     summarise(across(everything(), sum), .groups = "drop")
+#   
+#   edge_list_cbgs<-edgelist_cbgs %>% ungroup() %>% select(-Week)
+#   graph_cbgs <- graph_from_data_frame(edge_list_cbgs, directed = TRUE)
+#   return(graph_cbgs)  
+# }
+
+
+#Function to build a network for Zip codes. Needs x="file with visits".
+#This file must contain, at least, Week, home_cbg, dest_cbg, and totalVisits
+#It also uses the data frame "cbg_zip_mapping_TX" which must be loaded 
+#load("cbg_zip_mapping_TX.RData")
+make_net_zipcode<-function(x){
+  edgelist_zip<-x %>% select(Week,contains("cbg"),totalVisits) %>%
+    group_by(Week,home_cbg,dest_cbg) %>% 
+    summarise(across(everything(), sum), .groups = "drop")%>%
+    merge(cbg_zip_mapping_TX,by.x="home_cbg",by.y="cbgs") %>%
+    rename_at("ZCTA",~"home_ZCTA") %>% 
+    merge(cbg_zip_mapping_TX,by.x="dest_cbg",by.y="cbgs") %>%
+    rename_at("ZCTA",~"dest_ZCTA") %>% as_tibble() %>%
+    select(-contains("cbg")) %>% group_by(Week,home_ZCTA,dest_ZCTA) %>%
+    summarise(across(everything(), sum), .groups = "drop")
+  
+  edge_list<-edgelist_zip %>% ungroup() %>% select(-Week)
+  graph <- graph_from_data_frame(edge_list, directed = TRUE)
+  return(graph)  
+}
+
+#To have different normalizations for weights to compare
+diff_normalized_weights<-function(graph){
+  E(graph)$weight_inv <- 1 / E(graph)$totalVisits
+  # Normalize weights to the range [0, 1]
+  epsilon<-1e-6
+  E(graph)$weight_range <- (E(graph)$totalVisits - min(E(graph)$totalVisits)) / 
+    (max(E(graph)$totalVisits) - min(E(graph)$totalVisits))
+  E(graph)$weight_range[E(graph)$weight_range == 0] <- epsilon
+  # Normalize weights to sum to 1
+  E(graph)$weight_sum <- E(graph)$totalVisits / sum(E(graph)$totalVisits)
+  # Normalize weights by the maximum value
+  E(graph)$weight_max <- E(graph)$totalVisits / max(E(graph)$totalVisits)
+  return(graph)
+}
+
+#Stats for each node in the network
+node_metrics<-function(graph,y){
+  g=graph
+  weights <- edge_attr(g, y)
+  node_metrics <- data.frame(
+    node = V(g)$name,
+    in_degree = degree(g, mode = "in"),
+    out_degree = degree(g, mode = "out"),
+    in_strength = strength(g, mode = "in", weights = weights),
+    out_strength = strength(g, mode = "out", weights = weights),
+    bet_cent=betweenness(g, directed = TRUE, weights = weights),
+    clustering_local=transitivity(g, type = "local", weights = weights)
+  )
+  return(node_metrics)
+}
+
+#Stats for the whole network
+network_metrics<-function(graph,y){
+  g=graph
+  weight <- edge_attr(g, y)
+  network_metric<-data.frame(
+    Nodes=vcount(g),
+    size_SCC=max(components(g, mode = "strong")$csize),
+    size_WCC=max(components(g, mode = "weak")$csize),
+    mean_dist=mean_distance(g,weights = weight),
+    diameter=diameter(g, directed = TRUE, weights = weight),
+    clustering_global=transitivity(g, type = "global", weights = weight),
+    degree_assortativity=assortativity_degree(g, directed = TRUE),
+    density=edge_density(g, loops = FALSE)
+  )
+  return(network_metric)
+}
+
